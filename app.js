04df3b26f4accd169be27072ba4d839cb079d687
demosthenesst <127140991+demosthenesst@users.noreply.github.com>
2023-03-27 20:06:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ Application.prototype.start = function () {
   this.tuner.init();
 
   this.$a4.addEventListener("click", function () {
-    swal
-      .fire("Εισαγωγή συχνότητας της Α", { input: "number", inputValue: self.a4 })
+    swal.fire("Εισαγωγή συχνότητας")
+      .fire({input: "number", inputValue: self.a4 })
       .then(function ({ value: a4 }) {
         if (!parseInt(a4) || a4 === self.a4) {
           return;
